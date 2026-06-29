dc698d3fd0fcf73e0d0a7239e6b88332496c3e40
Abhishek <abhishek29112005@gmail.com>
2026-06-29 14:35:03 +0530

    add CollabCTA component for collaboration call-to-action

---
+++ src/components/CollabCTA.jsx
@@ -0,0 +1,9 @@
+import Box from "./Box"
+
+export default function CollabCTA({data}){
+    return(
+        <div>
+            <Box label={`Collabrations CTA`} data={data}/>
+        </div>
+    )
+}
