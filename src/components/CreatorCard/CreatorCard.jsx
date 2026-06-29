7346496f252ee02acbe879241ea25a6e1eba1205
Abhishek <abhishek29112005@gmail.com>
2026-06-29 14:35:56 +0530

    refactor CreatorCard component for improved layout and structure

--- src/components/CreatorCard/CreatorCard.jsx
+++ src/components/CreatorCard/CreatorCard.jsx
@@ -9,8 +9,8 @@ import {
 } from "../index";
 import { useParams } from "react-router";
 import { useSelector } from "react-redux";
-import {getProfile} from "../../store/CProfileSlice";
 import { useEffect } from "react";
+import CollabCTA from "../CollabCTA";
 
 export default function CreatorCard({ label }) {
      
@@ -19,12 +19,29 @@ export default function CreatorCard({ label }) {
     const data = useSelector((state)=>state?.profiles.filter((profile)=>profile.profileId == slug))
     const profile = data[0]
     return (
-    <div className="border border-[#1f1f1f] rounded-lg p-2 w-full h-full max-w-7xl">
-      <div className="w-full">
+    <div className="backdrop-blur bg-neutral-800/50 justify-start md:px-10 md:p-6 md:gap-6 flex flex-col  rounded-lg p-2 w-full h-full max-w-6xl text-neutral-50 gap-4 md:flex-row flex-wrap ">
+      <div className="w-full h-full px-2">
         <CreatorIntro data={profile}/>
       </div>
+      <div className=" justify-start lg:justify-around md:flex-start flex flex-col md:flex-row flex-wrap w-full h-full gap-4 border-neutral-500 bg-neutral-600/50 backdrop-blur-2xl border py-2 rounded-xl">
       <div>
-        
+        <AudienceInsights data={profile.audienceInsights}/>
+      </div>
+      <div>
+        <Contact data={profile.contact} />
+      </div>
+      <div>
+        <PerformanceMetics data={profile.performanceMetrics}/>
+      </div>
+      <div>
+        <SocialMediaOverview data={profile.socialMediaOverview}/>
+      </div>
+      <div>
+        <PreviousBrandCollabs data={profile.previousBrandCollaborations}/>
+      </div>
+      <div>
+        <CollabCTA data={profile.collaborationCTA}/>
+      </div>
       </div>
     </div>
   );
