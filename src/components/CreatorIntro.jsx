7b9af6e047a3770366eae0ad2d22eab8fd010485
Abhishek <abhishek29112005@gmail.com>
2026-06-29 14:35:19 +0530

    refactor CreatorIntro component for improved layout and styling

--- src/components/CreatorIntro.jsx
+++ src/components/CreatorIntro.jsx
@@ -1,15 +1,37 @@
 export default function CreatorIntro({ data }) {
-  console.log(data);
 
   return (
-    <div className="rounded-2xl w-full p-2  text-white flex flex-col gap-2 md:flex-row items-start">
+    <div className="rounded-2xl w-full flex flex-col gap-2 md:flex-row justify-between items-start h-full md:gap-10">
       <div className=" p-2 bg-linear-to-t from-black/70 to-black/10 rounded-2xl md:max-w-100 md:flex-row items-center w-full bg-white">
         <img src={data.image} alt="img" />
-        <div className="text-xl font-medium text-center relative bottom-5 bg-black/5 rounded-full backdrop-blur py-1">
+        <div className="text-xl font-medium text-center relative bottom-5 bg-black/5 rounded-full backdrop-blur ">
           {data.name.toUpperCase()}
         </div>
       </div>
-      <div className="intro pt-4 md:pl-7 md:text-lg">{data.creatorIntroduction.bio}</div>
+      <div className=" border border-neutral-300 rounded-xl p-4 md:pl-7 flex gap-4 flex-col h-full bg-neutral-500 justen">
+      <div className="intro md:text-lg flex flex-col gap-1">
+        <div className="underline decoration-neutral-50/50">
+      Creator Intro
+        </div>
+        {data.creatorIntroduction.bio}
+      </div>
+
+      <div className="h-full ">
+      <div className="border-b-gray-100/50 border-b">Location:{"  "}{data.creatorIntroduction.location}</div>
+      <div className="flex gap-2 border-b-gray-100/50 border-b"> Languages:
+        {data.creatorIntroduction.languages.map((lang) => (
+          <div key={lang}>{lang}</div>
+        ))}
+      </div>
+      <div className="flex gap-2 flex-wrap border-b-gray-100/50 border-b">ContentStyle: 
+        {data.creatorIntroduction.contentStyle.map((contentStyle) => (
+          <div key={contentStyle}>{contentStyle}</div>
+        ))}
+      </div>
+      <div className="border-b-gray-100/50 border-b"> Uploads: {data.creatorIntroduction.uploadsPerMonth}</div>
+      </div>
+      </div>
+      
     </div>
   );
 }
