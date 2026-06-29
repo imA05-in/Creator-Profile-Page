ade7577dfe8ec0288642bf867527dbfaddb2712b
Abhishek <abhishek29112005@gmail.com>
2026-06-29 14:34:57 +0530

    add AudienceInsights component with Box for data display

--- src/components/AudienceInsights.jsx
+++ src/components/AudienceInsights.jsx
@@ -1,5 +1,10 @@
-export default function AudienceInsights(){
-    return (<div>
-        AudIN
-    </div>)
+import Box from "./Box";
+
+export default function AudienceInsights({ data }) {
+
+  return (
+    <div className="w-full">
+    <Box label={"Audience Insights"} data={data}/>
+    </div>
+  );
 }
