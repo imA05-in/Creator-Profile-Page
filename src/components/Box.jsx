f10b281028113970e3cd2a9c334caeb10e50fe68
Abhishek <abhishek29112005@gmail.com>
2026-06-29 14:34:45 +0530

    add Box and BoxOWO components for structured data display

---
+++ src/components/Box.jsx
@@ -0,0 +1,18 @@
+export default function Box({data, label}){
+    return(
+        <div className="mx-2 bg-neutral-500 border border-neutral-300 rounded-2xl p-4 max-w-100">
+      <div className="underline decoration-neutral-50/50 pb-4">
+      {label}
+      </div>
+      <div className="flex flex-col gap-2">
+        {Object.entries(data).map((key) => (
+          <div 
+          className="border-b-gray-100/50 border-b"
+          key={key}>
+            {`${key[0]}: ${key[1]}`}
+          </div>
+        ))}
+      </div>
+    </div>
+    )
+}
---
+++ src/components/BoxOWO.jsx
@@ -0,0 +1,25 @@
+//box for objects withing objects
+
+export default function BoxOWO({ label, data }) {
+  return (
+    <div className="mx-2 bg-neutral-500 border border-neutral-300 rounded-2xl p-4 max-w-100">
+      <div className="underline decoration-neutral-50/50 pb-4">
+        {label}
+      </div>
+
+      {Object.entries(data).map((dat) => (
+        <div key={dat[0]} className="flex flex-col gap-2">
+          <div className="font-medium underline" >{dat[0]}</div> {/* platform names */}
+          {Object.entries(dat[1]).map((value) => (    
+            <div 
+            key={value}
+            className="border-b-gray-100/50 border-b "
+            >
+            {value[0] }{": "}{value[1]}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
