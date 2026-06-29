580c57f5de6457f7d904c89c64f2288143b3b2e4
Abhishek <abhishek29112005@gmail.com>
2026-06-29 14:36:10 +0530

    update App component styling and adjust background color in index.css for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 import Header from "./components/Header/Header";
 function App() {
   return (
-    <div className="h-full w-full bg-[#1f1f1f] text-white">
+    <div className="h-full w-full text-white">
       <Header />
       <main className="h-full p-2 flex flex-col items-center">
         <Outlet />
