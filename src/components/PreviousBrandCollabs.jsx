6149b1ec5b832ba9a9bf604115d5338116f1f681
Abhishek <abhishek29112005@gmail.com>
2026-06-29 14:35:35 +0530

    add PreviousBrandCollabs component to display previous brand collaborations using BoxOWO

--- src/components/PreviousBrandCollabs.jsx
+++ src/components/PreviousBrandCollabs.jsx
@@ -1,5 +1,8 @@
-export default function PreviousBrandCollabs(){
+import Box from "./Box";
+import BoxOWO from "./BoxOWO";
+
+export default function PreviousBrandCollabs({data}){
     return(<div>
-        PreviousBrandCollabs
+        <BoxOWO  label={`Previous Brand Collabs`} data={{" ":data[0], "":data[1]}}/>
     </div>)
 }
